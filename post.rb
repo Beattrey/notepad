74f1d28f26351bdebfb4ea05014945877159dba4
beattrey <beattrey1@gmail.com>
2021-08-31 15:49:11 +0300

    notepad v1, working

--- post.rb
+++ post.rb
@@ -1,5 +1,14 @@
 class Post
 
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+  end
+
+
   def initialize
     @created_at = Time.now
     @text = nil
@@ -11,10 +20,11 @@ class Post
 
   def to_strings
 
+
   end
 
   def save
-    file - File.new(file_path, "w:UTF-8")
+    file = File.new(file_path, "w:UTF-8")
 
     for item in to_strings do
       file.puts(item)
